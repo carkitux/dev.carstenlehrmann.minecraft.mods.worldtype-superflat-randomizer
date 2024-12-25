93e573debeb4f298a14b465d54032e962deb364e
Carsten Lehrmann <carsten-lehrmann.dev@outlook.com>
2024-12-25 11:30:15 +0100

    initial

---
+++ src/client/java/dev/carstenlehrmann/minecraft/mods/worldtype_superflat_randomizer/client/Worldtype_superflat_randomizerClient.java
@@ -0,0 +1,10 @@
+package dev.carstenlehrmann.minecraft.mods.worldtype_superflat_randomizer.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Worldtype_superflat_randomizerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/carstenlehrmann/minecraft/mods/worldtype_superflat_randomizer/Worldtype_superflat_randomizer.java
@@ -0,0 +1,10 @@
+package dev.carstenlehrmann.minecraft.mods.worldtype_superflat_randomizer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Worldtype_superflat_randomizer implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
